ac5be7f3cfbe6e33b47fd393e4182ea27b44442a
Hakan Vural <hakan@vuraldesign.com>
2019-04-11 02:44:21 +0300

    domain4 actions vccv added

--- _actions/domain4.actions.js
+++ _actions/domain4.actions.js
@@ -83,3 +83,29 @@ module.exports.run_turkish_four_cvvc = async function () {
         }
     }
 }
+module.exports.run_turkish_four_vccv = async function () {
+    let first_for = ["a", "e", "o", "u"];
+    let second_for = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "y", "z"];
+    let third_for = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "y", "z"];
+    let fourth_for = ["a", "e", "o", "u"];
+    let completed = 0;
+    console.log("Total Variations:" + first_for.length * second_for.length * third_for.length * fourth_for.length);
+
+    for (let i = 0; i < first_for.length; i++) {
+        for (let j = 0; j < second_for.length; j++) {
+            for (let k = 0; k < third_for.length; k++) {
+                for (let l = 0; l < fourth_for.length; l++) {
+                    var word = first_for[i] + "" + second_for[j] + "" + third_for[k] + "" + fourth_for[l] + ".com";
+                    await shell.run_dig_command(word, completed).then(resp => {
+                        if (resp != false) {
+                            domain4.add_new_domain4(resp.count, resp.domain);
+                        }
+                    }).catch((domain) => {
+                        domain4fail.add_new_domain4fail(completed, domain);
+                    });
+                    completed++;
+                }
+            }
+        }
+    }
+}
