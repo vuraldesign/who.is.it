be4eec1691cfcc8191cfe871fe6c8bc8017330f8
Hakan Vural <hakan@vuraldesign.com>
2019-04-11 02:17:11 +0300

    create shell commands

---
+++ _helpers/shell.commands.js
@@ -0,0 +1,48 @@
+const { exec } = require('child_process');
+
+module.exports.run_dig_command = async function(word, count) {
+    return new Promise(async function (resolve, reject) {
+        exec('dig ' + word + ' | grep -A1 \"ANSWER SECTION\\|AUTHORITY SECTION\\|PSEUDOSECTION\"', async function (err, stdout, stderr) {
+            if (err) {
+                return reject(word);
+            } else {
+                if (!stderr) {
+                    if (stdout.indexOf("AUTHORITY SECTION") > -1 | stdout.indexOf("PSEUDOSECTION") > -1) {
+                        await run_whois_command(word, count).then((resp) => {
+                            return resolve(resp);
+                        }).catch(() => {
+                            return reject(word);
+                        });
+                    } else {
+                        return resolve(false);
+                    }
+                } else {
+                    return reject(word);
+                }
+            }
+        });
+    });
+}
+module.exports.run_whois_command = function(word, count) {
+    return new Promise(function (resolve, reject) {
+        exec('whois ' + word + ' | grep -A1 \"Domain Name\\|No match for domain\"', (err, stdout, stderr) => {
+            if (err) {
+                return reject(word);
+            } else {
+                if (!stderr) {
+                    if (stdout.indexOf("No match for domain") > -1) {
+                        return resolve({
+                            "count": count,
+                            "domain": word
+                        });
+                    } else {
+                        return resolve(false);
+                    }
+                    return resolve("OK");
+                } else {
+                    return reject(word);
+                }
+            }
+        });
+    });
+}
