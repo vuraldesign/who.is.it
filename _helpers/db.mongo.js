9f2646a25967cf0237ac5fbea308841cf60b4961
Hakan Vural <hakan@vuraldesign.com>
2019-04-11 02:10:59 +0300

    update mongo options

--- _helpers/db.mongo.js
+++ _helpers/db.mongo.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 var url = "YOUR_LOCAL_CONNECTION_STRING";
-var url = 'mongodb://localhost:27017/?3t.ssh=true&3t.sshAddress=185.169.52.54&3t.sshPort=22&3t.sshAuthMode=password&3t.sshUser=ortak&3t.sshPassword=Caha*0192!'; //TODO
 const mongo_options = {
     reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 500,
-    poolSize: 100,
+    reconnectInterval: 50,
+    poolSize: 10,
     bufferMaxEntries: 0,
     useNewUrlParser: true
 };
