001f5f3095db9a1466c04ccbf93565a86c947d57
Hakan Vural <hakan@vuraldesign.com>
2019-04-11 02:36:13 +0300

    domain5 fail models added

---
+++ _models/domain5fail.models.js
@@ -0,0 +1,27 @@
+var Domain5Fail = require('../_structures/domain45ail.structures');
+
+exports.get_domain5fail_list = function () {
+    return new Promise(function (resolve, reject) {
+        Domain5Fail.find({}, function (err, nes) {
+            if (err) {
+                return reject(err);
+            }
+            else {
+                return resolve(nes);
+            }
+        });
+    });
+}
+exports.add_new_domain4fail = function (count, domain) {
+    return new Promise(function (resolve, reject) {
+        let domain5fail = new Domain5Fail({ count, domain });
+        domain5fail.save(function (err) {
+            if (err) {
+                return reject(err);
+            }
+            else {
+                return resolve("Domain Added");
+            }
+        });
+    });
+}
