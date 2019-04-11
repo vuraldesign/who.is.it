285cbf004258e860d5e038615a8559d374f3c0fb
Hakan Vural <hakan@vuraldesign.com>
2019-04-11 12:10:08 +0300

    main file TODO update

--- main.js
+++ main.js
@@ -4,15 +4,8 @@ var domain5 = require('./_actions/domain5.actions');
 
 async function search_start(){
     db.connect(true).then(() => { //Local Mongo connect if u dont remote set false
-        //domain4.run_turkish_four_cvcv();TO DO
-        //domain4.run_turkish_four_cvvc();TO DO
-        //domain4.run_turkish_four_vccv();TO DO
-        //domain4.run_turkish_four_vcvc();TO DO
+        domain4.run_turkish_four_cvcv();
 
-        //domain5.run_turkish_five_cvcvc();TO DO
-        //domain5.run_turkish_five_cvvcv();TO DO
-        //domain5.run_turkish_five_vccvc();TO DO
-        domain5.run_turkish_five_vcvcv();
     }).catch((err) => {
         console.log("Project not start.");
         console.log("Please check your mongodb connection and mongo connection string in helpers/mongo.js");
