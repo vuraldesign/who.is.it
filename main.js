dca69bcc8e009eeeba54170f2dc3189fb2451bf0
Hakan Vural <hakan@vuraldesign.com>
2019-10-22 11:44:21 +0300

    add to do

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var domain5 = require('./_actions/domain5.actions');
 
 async function search_start(){
     db.connect(true).then(() => { //Local Mongo connect if u dont remote set false
+        domain5.run_turkish_five_vcvcv();
     }).catch((err) => {
         console.log("Project not start.");
         console.log("Please check your mongodb connection and mongo connection string in helpers/mongo.js");
